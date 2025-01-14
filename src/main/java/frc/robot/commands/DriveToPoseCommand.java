0033d906a9ee03ab758a9b03fd2ff33e40695b72
M&M <teamautomatons2823@gmail.com>
2025-01-14 15:52:32 -0600

    Added Drive subsystem

--- src/main/java/frc/robot/commands/DriveToPoseCommand.java
+++ src/main/java/frc/robot/commands/DriveToPoseCommand.java
@@ -4,29 +4,19 @@
 
 package frc.robot.commands;
 
-import java.io.IOException;
-
-import org.json.simple.parser.ParseException;
-
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.FollowPathCommand;
 import com.pathplanner.lib.commands.PathfindingCommand;
-import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.util.FileVersionException;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
-import edu.wpi.first.math.controller.HolonomicDriveController;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
-import frc.robot.Constants.RobotConfigConstants;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import com.pathplanner.lib.commands.PathfindThenFollowPath;
 
@@ -37,45 +27,27 @@ public class DriveToPoseCommand extends Command {
   String m_pathName;
   PathPlannerPath m_path;
   PathfindingCommand m_PathfindingCommand;
-  PathfindThenFollowPath m_pathPlannerCommand;
-  HolonomicDriveController m_controller;
-  RobotConfig m_config;
+  PathfindThenFollowPathHolonomic m_pathPlannerCommand;
 
   public DriveToPoseCommand(DriveSubsystem driveSubsystem, String pathName) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_driveSubsystem = driveSubsystem;
-    try{
-      //m_config = RobotConfig.fromGUISettings();
-    m_config = new RobotConfig(RobotConfigConstants.massKG,RobotConfigConstants.MOI,RobotConfigConstants.moduleConfig,RobotConfigConstants.moduleOffsets);
-    } catch (Exception e) {
-      // Handle exception as needed
-      e.printStackTrace();
-    }
     m_pathName = pathName;
-    try {
-      m_path = PathPlannerPath.fromPathFile(pathName);
-    }
-    catch(Exception e) {
-      System.out.println("Path Exception");
-    }
+    m_path = PathPlannerPath.fromPathFile(pathName);
     m_pathPlannerCommand = PathCommand();
     addRequirements(driveSubsystem);
   }
-  public PathfindThenFollowPath PathCommand() {
-    return new PathfindThenFollowPath(
+  public PathfindThenFollowPathHolonomic PathCommand() {
+    return new PathfindThenFollowPathHolonomic(
         m_path,
-        new PathConstraints(Constants.AutoConstants.kMaxAutoVelocity,
-            Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared,
-            Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond,
-            Constants.AutoConstants.kMaxAngularAcceleration),
+        new PathConstraints(AutoConstants.kMaxAutoVelocity,
+            AutoConstants.kMaxAccelerationMetersPerSecondSquared,
+            AutoConstants.kMaxAngularSpeedRadiansPerSecond,
+            AutoConstants.kMaxAngularAcceleration),
         m_driveSubsystem::getPose,
         m_driveSubsystem::getCurrentspeeds,
         m_driveSubsystem::driveRobotRelative,
-        m_controller = new HolonomicDriveController(
-  new PIDController(1, 0, 0), new PIDController(1, 0, 0),
-  new ProfiledPIDController(1, 0, 0,
-    new TrapezoidProfile.Constraints(6.28, 3.14))),
-        m_config,
+        DriveConstants.holonomicConfig,
         () -> {
           // Boolean supplier that controls when the path will be mirrored for the red alliance
           // This will flip the path being followed to the red side of the field.
@@ -101,27 +73,14 @@ public class DriveToPoseCommand extends Command {
   @Override
   public void execute() {
     m_pathPlannerCommand.execute();
-    PathPlannerPath path;
-    try {
-      path = PathPlannerPath.fromPathFile("UpperReef3Coral");
-    } catch (FileVersionException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (ParseException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    PathPlannerPath path = PathPlannerPath.fromPathFile("UpperReef3Coral");
     PathConstraints constraints = new PathConstraints(
         3.0, 4.0,
         Units.degreesToRadians(540), Units.degreesToRadians(720));
     Command pathfindingCommand = AutoBuilder.pathfindThenFollowPath(
-        m_path,
+        path,
         constraints);
     AutoBuilder.pathfindThenFollowPath(m_path, constraints).schedule();
-
   }
 
   // Called once the command ends or is interrupted.
