d7f4f67b5680cd17dcb1369b12e5d1ad19a09631
einstein2823 <automatons2823@gmail.com>
2025-01-16 15:36:15 -0600

    intake subsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class IntakeSubsystem {
+    public IntakeSubsystem() {
+        
+    }
+}
