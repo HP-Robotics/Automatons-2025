6e651650b87049688f90cfd12825b2ee8a679a50
M&M <teamautomatons2823@gmail.com>
2025-02-14 17:05:22 -0600

    Comment out intake beam break

--- src/main/java/frc/robot/subsystems/InNOutSubsystem.java
+++ src/main/java/frc/robot/subsystems/InNOutSubsystem.java
@@ -1,13 +1,10 @@
 package frc.robot.subsystems;
 
 import frc.robot.BeamBreak;
-import frc.robot.Constants.ControllerConstants;
 import frc.robot.Constants.IDConstants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.OuttakeConstants;
 import frc.robot.Constants.PortConstants;
-
-import java.time.Instant;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
@@ -15,17 +12,16 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTableValue;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class InNOutSubsystem extends SubsystemBase {
     TalonFX m_intakeMotor = new TalonFX(IDConstants.intakeMotorID);
     TalonFX m_outtakeMotor = new TalonFX(IDConstants.outtakeMotorID);
     public TalonFX m_intakeFoldMotor = new TalonFX(IDConstants.intakeFoldMotorID);
-    BeamBreak m_intakeBeamBreak = new BeamBreak(0); // Is in robot TODO: Find channel id and remove fakeBeamBreak
+    // BeamBreak m_intakeBeamBreak = new BeamBreak(0); // Is in robot TODO: Find
+    // channel id
     public String m_state = "empty";
     NetworkTable m_table;
     BeamBreak m_outtakeBeamBreak; // Checks to see if can score
@@ -33,7 +29,7 @@ public class InNOutSubsystem extends SubsystemBase {
 
     public InNOutSubsystem() {
         m_table = NetworkTableInstance.getDefault().getTable("InNOutSubsystem");
-        m_intakeBeamBreak = new BeamBreak(PortConstants.intakeBeamBreakID);
+        // m_intakeBeamBreak = new BeamBreak(PortConstants.intakeBeamBreakID);
         m_outtakeBeamBreak = new BeamBreak(PortConstants.outtakeBeamBreakID); // TODO: find the real channel IDs
         m_elevatorBeamBreak = new BeamBreak(PortConstants.elevatorBeamBreakID);
     }
@@ -58,13 +54,14 @@ public class InNOutSubsystem extends SubsystemBase {
 
     public void periodic() {
         m_table.putValue("state", NetworkTableValue.makeString(m_state));
-        m_table.putValue("Intake Beam Broken", NetworkTableValue.makeBoolean(m_intakeBeamBreak.beamBroken()));
+        // m_table.putValue("Intake Beam Broken",
+        // NetworkTableValue.makeBoolean(m_intakeBeamBreak.beamBroken()));
         m_table.putValue("Outtake Beam Broken", NetworkTableValue.makeBoolean(m_outtakeBeamBreak.beamBroken()));
         m_table.putValue("Elevator Beam Broken", NetworkTableValue.makeBoolean(m_elevatorBeamBreak.beamBroken()));
     }
 
     public boolean intakeHasCoral() {
-        return m_elevatorBeamBreak.beamBroken() || m_intakeBeamBreak.beamBroken();
+        return m_elevatorBeamBreak.beamBroken(); // || m_intakeBeamBreak.beamBroken();
     }
 
     public boolean isLoaded() {
@@ -72,7 +69,8 @@ public class InNOutSubsystem extends SubsystemBase {
     }
 
     public boolean isEmpty() {
-        return !m_elevatorBeamBreak.beamBroken() && !m_intakeBeamBreak.beamBroken() && !m_outtakeBeamBreak.beamBroken();
+        return !m_elevatorBeamBreak.beamBroken()
+                /* && !m_intakeBeamBreak.beamBroken() */ && !m_outtakeBeamBreak.beamBroken();
     }
 
     public Command IntakeCoral() {
