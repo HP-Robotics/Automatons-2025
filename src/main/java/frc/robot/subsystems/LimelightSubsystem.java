04093384209e1b59493e591016bf206f441ee451
einstein2823 <teamautomatons2823@gmail.com>
2025-03-18 16:16:10 -0500

    two limelight setup

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -4,42 +4,29 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.DoubleArraySubscriber;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableValue;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.networktables.TimestampedDoubleArray;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.Constants.LimelightConstants;
 
 public class LimelightSubsystem extends SubsystemBase {
-  NetworkTableEntry botPoseBlue; // Red will be mirrored from this
   CommandJoystick m_driveJoystick;
 
-  public final Field2d m_field = new Field2d();
-  NetworkTable m_table;
-  public double tx;
-  public double ty;
-  public double tz;
-  public double rx;
-  public double ry;
-  public double rz;
-  public boolean m_seeingAprilTag;
-  public boolean m_aprilTagSeen;
-  public int m_targetAprilTagID;
-  final DoubleArraySubscriber limeSub;
+  NetworkTable m_leftTable;
+  NetworkTable m_rightTable;
+  public int targetAprilTagID;
+  private final DoubleArraySubscriber m_leftSub;
+  private final DoubleArraySubscriber m_rightSub;
   private double[] defaultValues = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-
-  public Pose2d m_visionPose2d = new Pose2d();
-  public Pose2d m_targetPoseRelative;
 
   NetworkTableInstance inst = NetworkTableInstance.getDefault();
   NetworkTable poseEstimatorTable = inst.getTable("pose-estimator-table");
@@ -49,17 +36,14 @@ public class LimelightSubsystem extends SubsystemBase {
   PoseEstimatorSubsystem m_poseEstimator;
 
   public LimelightSubsystem(PoseEstimatorSubsystem poseEstimatorSubsystem) {
-    Shuffleboard.getTab("shuffleboard")
-        .add("Pose2d", m_field)
-        .withWidget(BuiltInWidgets.kField);
-    m_table = NetworkTableInstance.getDefault().getTable("limelight-kite");
-    m_table.getEntry("imumode_set").setDouble(0);
-    botPoseBlue = m_table.getEntry("botpose_orb_wpiblue");
+    m_leftTable = NetworkTableInstance.getDefault().getTable("limelight-shpwrte");
+    m_rightTable = NetworkTableInstance.getDefault().getTable("limelight-kite");
+    m_leftTable.getEntry("imumode_set").setDouble(0);
+    m_rightTable.getEntry("imumode_set").setDouble(0); // 4 is scary
     m_poseEstimator = poseEstimatorSubsystem;
     publisher = poseEstimatorTable.getStructTopic("AprilTag Pose", Pose2d.struct).publish();
-    limeSub = m_table.getDoubleArrayTopic("botpose_orb_wpiblue").subscribe(defaultValues);
-
-    m_aprilTagSeen = false;
+    m_leftSub = m_leftTable.getDoubleArrayTopic("botpose_orb_wpiblue").subscribe(defaultValues);
+    m_rightSub = m_rightTable.getDoubleArrayTopic("botpose_orb_wpiblue").subscribe(defaultValues);
   }
 
   public double getDistanceToPose(Pose2d robot, Pose2d fieldPose) {
@@ -89,6 +73,7 @@ public class LimelightSubsystem extends SubsystemBase {
     return output;
   }
 
+  public void doLimelight(NetworkTable limelightTable, DoubleArraySubscriber botPoseSub) {
     double[] robotOrientationEntries = new double[6];
     robotOrientationEntries[0] = m_poseEstimator.getPose().getRotation().getDegrees();
     robotOrientationEntries[1] = 0;
@@ -96,52 +81,55 @@ public class LimelightSubsystem extends SubsystemBase {
     robotOrientationEntries[3] = 0;
     robotOrientationEntries[4] = 0;
     robotOrientationEntries[5] = 0;
-    m_table.getEntry("robot_orientation_set").setDoubleArray(robotOrientationEntries);
+    limelightTable.getEntry("robot_orientation_set").setDoubleArray(robotOrientationEntries);
 
     double[] botPose = null;
     double latency = 0;
     double timeStamp = 0;
-    m_seeingAprilTag = m_table.getEntry("tv").getDouble(0) != 0;
-    botPose = botPoseBlue.getDoubleArray(defaultValues);
-    tx = m_table.getEntry("tx").getDouble(0);
-    ty = m_table.getEntry("ty").getDouble(0);
 
-    m_targetPoseRelative = new Pose2d(tx, ty, new Rotation2d());
+    for (TimestampedDoubleArray tsValue : botPoseSub.readQueue()) {
+      botPose = tsValue.value;
+      if ((int) botPose[7] == 0) {
+        continue;
+      }
+      timeStamp = Timer.getFPGATimestamp(); // TODO: tsValue.timestamp; test this
+      double botPosX = botPose[0];
+      double botPosY = botPose[1];
+      // double botPosZ = botpose[2];
+      // double botRotX = botpose[3];
+      // double botRotY = botpose[4]; // TODO: Store botPosZ, botRotX, and botRotY
+      // somewhere (Store 3D)
+      double botRotZ = botPose[5];
+      latency = botPose[6];
+      targetAprilTagID = (int) botPose[11];
 
-    m_field.setRobotPose(m_targetPoseRelative);
-
-    if (m_seeingAprilTag) {
-      m_targetAprilTagID = (int) m_table.getEntry("tid").getInteger(0);
-      for (TimestampedDoubleArray tsValue : limeSub.readQueue()) {
-        botPose = tsValue.value;
-        timeStamp = Timer.getFPGATimestamp();
-        double botPosX = botPose[0];
-        double botPosY = botPose[1];
-        // double botPosZ = botpose[2];
-        // double botRotX = botpose[3];
-        // double botRotY = botpose[4]; // TODO: Store botPosZ, botRotX, and botRotY
-        // somewhere (Store 3D)
-        double botRotZ = botPose[5];
-        latency = botPose[6];
-
-        timeStamp -= latency / 1000;
-        m_targetPoseRelative = new Pose2d(botPosX, botPosY, new Rotation2d(Math.toRadians(botRotZ)));
-        m_visionPose2d = m_targetPoseRelative;
-        if (m_poseEstimator != null && 0 <= m_targetAprilTagID && m_targetAprilTagID <= 22) {
-          if (botPosX != 0 || botPosY != 0 || botRotZ != 0) {
-            publisher.set(m_targetPoseRelative);
-            Rotation2d aprilTagToRobotAngle = Rotation2d.fromDegrees(
-                getAngleToPose(m_targetPoseRelative, LimelightConstants.aprilTagList[m_targetAprilTagID]) - 180);
-            double angleDiff = Math.abs(LimelightConstants.aprilTagList[m_targetAprilTagID].getRotation()
-                .minus(aprilTagToRobotAngle)
-                .getRadians());
-            m_poseEstimator.updateVision(m_targetPoseRelative, timeStamp,
-                getDistanceToPose(m_targetPoseRelative, LimelightConstants.aprilTagList[m_targetAprilTagID]),
-                angleDiff);
-            // System.out.println("saw apriltag: " + timeStamp + " latency: " + latency);
-          }
+      timeStamp -= latency / 1000;
+      Pose2d targetPoseRelative = new Pose2d(botPosX, botPosY, new Rotation2d(Math.toRadians(botRotZ)));
+      if (m_poseEstimator != null && 0 <= targetAprilTagID
+          && targetAprilTagID < LimelightConstants.aprilTagList.length) {
+        limelightTable.putValue("botPosX", NetworkTableValue.makeDouble(botPosX));
+        limelightTable.putValue("botPosY", NetworkTableValue.makeDouble(botPosY));
+        limelightTable.putValue("botPosZ", NetworkTableValue.makeDouble(botRotZ));
+        if (botPosX != 0 || botPosY != 0 || botRotZ != 0) {
+          publisher.set(targetPoseRelative);
+          Rotation2d aprilTagToRobotAngle = Rotation2d.fromDegrees(
+              getAngleToPose(targetPoseRelative, LimelightConstants.aprilTagList[targetAprilTagID]) - 180);
+          limelightTable.putValue("Angle to pose", NetworkTableValue
+              .makeDouble(getAngleToPose(targetPoseRelative, LimelightConstants.aprilTagList[targetAprilTagID]) - 180));
+          double angleDiff = Math.abs(LimelightConstants.aprilTagList[targetAprilTagID].getRotation()
+              .minus(aprilTagToRobotAngle)
+              .getRadians());
+          m_poseEstimator.updateVision(targetPoseRelative, timeStamp,
+              getDistanceToPose(targetPoseRelative, LimelightConstants.aprilTagList[targetAprilTagID]),
+              angleDiff);
+          // System.out.println("saw apriltag: " + timeStamp + " latency: " + latency);
         }
       }
     }
+  }
+
+  @Override
+  public void periodic() {
+    doLimelight(m_rightTable, m_rightSub);
   }
 }
